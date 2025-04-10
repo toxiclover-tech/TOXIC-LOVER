f414d1a54acd0c66b051a416559e7be7c189ea49
toxiclover-tech <Alonetech111@gmail.com>
2025-04-10 17:43:11 +0300

    .

--- pair.js
+++ pair.js
@@ -50,9 +50,9 @@ router.get('/', async (req, res) => {
                     lastDisconnect
                 } = s;
                 if (connection == "open") {
-                await delay(5000);
+                await delay(50000);
                 let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
-                await delay(800);
+                await delay(8000);
                let b64data = Buffer.from(data).toString('base64');
                let session = await Pair_Code_By_France_King.sendMessage(Pair_Code_By_France_King.user.id, { text: ''+ b64data });
 
